8a13c270a649a768ab81661a218873099ab3212b
jana <bkeyh382@gmail.com>
2026-03-01 11:56:50 +0300

    Update base_model.py

--- part3/app/models/base_model.py
+++ part3/app/models/base_model.py
@@ -1,1 +1,17 @@
+from app.extensions import db
+from datetime import datetime
+import uuid
 
+class BaseModel(db.Model):
+    __abstract__ = True
+    
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'created_at': self.created_at.isoformat() if self.created_at else None,
+            'updated_at': self.updated_at.isoformat() if self.updated_at else None
+        }
