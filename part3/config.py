7dbae939647b7edd7581daea55d41ec8f087dbeb
csrama <156553209+csrama@users.noreply.github.com>
2026-03-03 00:58:52 +0300

    Update config.py

--- part3/config.py
+++ part3/config.py
@@ -1,21 +1,11 @@
 import os
-from datetime import timedelta
 
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key-789')
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwt-secret-key-456')
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=2)
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///hbnb_dev.db'
-
-class DevelopmentConfig(Config):
+class DevelopmentConfig:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///hbnb_dev.db'
+    SQLALCHEMY_DATABASE_URI = "sqlite:///hbnb.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = "super-secret-key"
 
-class TestingConfig(Config):
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///hbnb_test.db'
-
-class ProductionConfig(Config):
-    DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///hbnb_prod.db')
+config_dict = {
+    "development": DevelopmentConfig
+}
