ea7c49a44e98e558ab7043f6904c541228734417
Raghad717 <r4gh4dalmalki@gmail.com>
2026-03-10 01:13:06 +0300

    Add GET method to retrieve all amenities

--- part3/app/api/v1/amenities.py
+++ part3/app/api/v1/amenities.py
@@ -9,15 +9,20 @@ facade = HBnBFacade()
 amenity_model = api.model('Amenity', {
     'name': fields.String(required=True),
 })
-
 @api.route('/')
 class AmenityList(Resource):
+
+    def get(self):
+        amenities = facade.get_all_amenities()
+        return [a.to_dict() for a in amenities], 200
+
     @jwt_required()
     @api.expect(amenity_model)
     def post(self):
         claims = get_jwt()
         if not claims.get('is_admin'):
             return {'error': 'Admin only'}, 403
+
         data = request.get_json()
         amenity = facade.create_amenity(**data)
         return amenity.to_dict(), 201
