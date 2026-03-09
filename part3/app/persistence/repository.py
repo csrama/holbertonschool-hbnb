effd3eefc8ffac73ff786cb62775eec42ed99ed3
Raghad717 <r4gh4dalmalki@gmail.com>
2026-03-10 02:55:12 +0300

    Update repository.py

--- part3/app/persistence/repository.py
+++ part3/app/persistence/repository.py
@@ -1,40 +1,53 @@
 from app.extensions import db
+from app.models.user import User
 from app.models.place import Place
 from app.models.review import Review
 from app.models.amenity import Amenity
 
+class UserRepository:
+    def get_by_email(self, email):
+        return User.query.filter_by(email=email).first()
+
+    def get_by_id(self, user_id):
+        return User.query.get(user_id)
+
+    def create(self, user):
+        db.session.add(user)
+        db.session.commit()
+        return user
+
 class PlaceRepository:
-    def create(self, place: Place):
+    def get_all(self):
+        return Place.query.all()
+
+    def get_by_id(self, place_id):
+        return Place.query.get(place_id)
+
+    def create(self, place):
         db.session.add(place)
         db.session.commit()
         return place
 
-    def get_by_id(self, place_id: str):
-        return Place.query.get(place_id)
-
-    def get_all(self):
-        return Place.query.all()
-
 class ReviewRepository:
-    def create(self, review: Review):
+    def get_all(self):
+        return Review.query.all()
+
+    def get_by_id(self, review_id):
+        return Review.query.get(review_id)
+
+    def create(self, review):
         db.session.add(review)
         db.session.commit()
         return review
 
-    def get_by_id(self, review_id: str):
-        return Review.query.get(review_id)
-
-    def get_all(self):
-        return Review.query.all()
-
 class AmenityRepository:
-    def create(self, amenity: Amenity):
+    def get_all(self):
+        return Amenity.query.all()
+
+    def get_by_id(self, amenity_id):
+        return Amenity.query.get(amenity_id)
+
+    def create(self, amenity):
         db.session.add(amenity)
         db.session.commit()
         return amenity
-
-    def get_by_id(self, amenity_id: str):
-        return Amenity.query.get(amenity_id)
-
-    def get_all(self):
-        return Amenity.query.all()
