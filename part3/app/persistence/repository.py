fef4192352a465df696bd0754e7292b08d029f52
Raghad717 <r4gh4dalmalki@gmail.com>
2026-03-10 05:14:37 +0300

    Update repository.py

--- part3/app/persistence/repository.py
+++ part3/app/persistence/repository.py
@@ -1,53 +1,41 @@
+# app/persistence/repository.py
 from app.extensions import db
 from app.models.user import User
 from app.models.place import Place
 from app.models.review import Review
 from app.models.amenity import Amenity
 
-class UserRepository:
+class BaseRepository:
+    """Generic repository for CRUD operations"""
+    def __init__(self, model):
+        self.model = model
+
+    def get_by_id(self, obj_id):
+        return self.model.query.get(obj_id)
+
+    def get_all(self):
+        return self.model.query.all()
+
+    def create(self, obj):
+        db.session.add(obj)
+        db.session.commit()
+        return obj
+
+class UserRepository(BaseRepository):
+    def __init__(self):
+        super().__init__(User)
+
     def get_by_email(self, email):
-        return User.query.filter_by(email=email).first()
+        return self.model.query.filter_by(email=email).first()
 
-    def get_by_id(self, user_id):
-        return User.query.get(user_id)
+class PlaceRepository(BaseRepository):
+    def __init__(self):
+        super().__init__(Place)
 
-    def create(self, user):
-        db.session.add(user)
-        db.session.commit()
-        return user
+class ReviewRepository(BaseRepository):
+    def __init__(self):
+        super().__init__(Review)
 
-class PlaceRepository:
-    def get_all(self):
-        return Place.query.all()
-
-    def get_by_id(self, place_id):
-        return Place.query.get(place_id)
-
-    def create(self, place):
-        db.session.add(place)
-        db.session.commit()
-        return place
-
-class ReviewRepository:
-    def get_all(self):
-        return Review.query.all()
-
-    def get_by_id(self, review_id):
-        return Review.query.get(review_id)
-
-    def create(self, review):
-        db.session.add(review)
-        db.session.commit()
-        return review
-
-class AmenityRepository:
-    def get_all(self):
-        return Amenity.query.all()
-
-    def get_by_id(self, amenity_id):
-        return Amenity.query.get(amenity_id)
-
-    def create(self, amenity):
-        db.session.add(amenity)
-        db.session.commit()
-        return amenity
+class AmenityRepository(BaseRepository):
+    def __init__(self):
+        super().__init__(Amenity)
