9aab5a55a448d5a01ce698edaeef85c47f041af9
jana <bkeyh382@gmail.com>
2026-03-13 19:09:26 +0300

    Update run.py

--- part3/run.py
+++ part3/run.py
@@ -3,6 +3,8 @@ from app.extensions import db, bcrypt, jwt
 from app.api.v1.auth import auth_bp
 from app.api.v1.users import api as users_api
 from app.api.v1.places import api as places_api
+from app.api.v1.amenities import api as amenities_api
+from app.api.v1.reviews import api as reviews_api
 from flask_restx import Api
 
 app = Flask(__name__)
@@ -10,18 +12,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hbnb.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'super-secret-key'
 
-# Initialize extensions
 db.init_app(app)
 bcrypt.init_app(app)
 jwt.init_app(app)
 
-# Register Blueprints
 app.register_blueprint(auth_bp, url_prefix='/api/v1/auth')
 
-# Register Namespaces
 api = Api(app)
 api.add_namespace(users_api, path='/api/v1/users')
 api.add_namespace(places_api, path='/api/v1/places')
+api.add_namespace(amenities_api, path='/api/v1/amenities')
+api.add_namespace(reviews_api, path='/api/v1/reviews')
 
 if __name__ == '__main__':
     with app.app_context():
